7421a8f2050225da2bbd7f18e2f188251e187818
batchanhdao <anhle12341710@gmail.com>
2024-05-23 08:58:17 +0700

    anhle

--- python/app_python/image_to_text/main.py
+++ python/app_python/image_to_text/main.py
@@ -9,12 +9,31 @@ path_to_pytesseract = r"C:\Users\Admin\AppData\Local\Programs\Tesseract-OCR\tess
 pt.tesseract_cmd = path_to_pytesseract
 
 # Thiết lập thư mục làm việc
-working_directory = r"G:\Máy tính khác\My Laptop\pythonProject\python\app_python\image_to_text"
+working_directory = r"D:\pythonProject\python\app_python\image_to_text"
 os.chdir(working_directory)
 print(f"Current working directory: {os.getcwd()}")
 
 # Mở file để ghi kết quả
 output_file = open('text.txt', '+a', encoding='UTF-8')
+
+def preprocess_image(img):
+    # Chuyển đổi ảnh sang không gian màu HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+    # Phân tách các kênh màu
+    h, s, v = cv2.split(hsv)
+
+    # Tăng cường độ chói bằng cách tăng giá trị kênh V
+    v += 50  # Điều chỉnh giá trị này tùy thuộc vào ảnh cụ thể
+
+    # Gộp các kênh màu lại
+    enhanced_hsv = cv2.merge((h, s, v))
+
+    # Chuyển đổi trở lại không gian màu RGB
+    enhanced_rgb = cv2.cvtColor(enhanced_hsv, cv2.COLOR_HSV2RGB)
+
+    return enhanced_rgb
+
 
 while True:
     try:
@@ -31,13 +50,17 @@ while True:
             img_cv = cv2.imread(path_to_image)
             img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
 
+            # Tiền xử lý ảnh để tăng cường độ chói của số
+            processed_img = preprocess_image(img_rgb)
+
             # Chọn ngôn ngữ: 'eng' hoặc 'vie'
             language = input("Select language ('eng' or 'vie'): ").strip().lower()
 
             if language == 'vie':
-                text = pt.image_to_string(image=img_rgb, lang='vie')
+                text = pt.image_to_string(image=processed_img, lang='vie', config='--psm 7 --oem 3')
             else:    
-                text = pt.image_to_string(image=img_rgb, lang='eng')
+                # Sử dụng config='--psm 6' để nhận dạng các ký tự đơn lẻ
+                text = pt.image_to_string(image=processed_img, lang='eng', config='--psm 6 --oem 3')
 
             # Xử lý và ghi kết quả vào file
             lines = [line.strip() for line in str(text).strip().split('\n') if line.strip()]
@@ -51,5 +74,5 @@ while True:
 
 # Đóng file khi hoàn thành
 output_file.close()
-# cv2.imshow("img", img_rgb)
+# cv2.imshow("img", processed_img)
 # cv2.waitKey(0)
